6d6781a68ede5bb029da03303dae2fa54fe2a548
pieterjan84 <pieterjan84@users.noreply.github.com>
2024-11-26 11:11:37 +0000

    federated-voting: extracted trust-graph to separate component

---
+++ apps/frontend/src/components/federated-voting/trust-graph/trust-graph.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="card graph">
+    <div class="card-body pt-4 pb-0">
+      <div
+        class="d-flex justify-content-around align-items-baseline border-bottom pt-0 pb-4"
+      >
+        <div class="title mb-0 pb-0">Trust Graph</div>
+      </div>
+      <Graph
+        ref="graph"
+        :selected-vertices="selectedVertices"
+        style="height: 400px"
+        :full-screen="false"
+        :view-graph="viewGraph"
+        :is-loading="false"
+        :option-show-failing-edges="true"
+        :option-highlight-trusting-nodes="true"
+        :option-highlight-trusted-nodes="true"
+        :option-show-regular-edges="true"
+        :option-transitive-quorum-set-only="false"
+        :zoom-enabled="false"
+        :initial-zoom="2"
+        :propagation-enabled="true"
+        @vertex-selected="handleVertexSelected"
+      />
+    </div>
+    <div class="card-footer pt-0 footer"></div>
+  </div>
+</template>
+<script setup lang="ts">
+import Graph from "@/components/visual-navigator/graph/graph.vue";
+import ViewGraph from "@/components/visual-navigator/graph/view-graph";
+import ViewVertex from "@/components/visual-navigator/graph/view-vertex";
+import { federatedVotingStore } from "@/store/useFederatedVotingStore";
+import { Network, TrustGraph } from "shared";
+import { ref, onMounted } from "vue";
+
+const viewGraph = ref<ViewGraph>(new ViewGraph());
+const selectedVertices = ref<ViewVertex[]>([]);
+
+const trustGraph = federatedVotingStore.network.nodesTrustGraph;
+
+onMounted(() => {
+  viewGraph.value = ViewGraph.fromNodes(
+    federatedVotingStore.network as Network,
+    trustGraph as TrustGraph,
+    viewGraph.value,
+  );
+});
+
+const handleVertexSelected = (vertex: ViewVertex) => {
+  selectedVertices.value = [vertex];
+  federatedVotingStore.selectedNodeId = vertex.key;
+};
+</script>
+<style scoped>
+.footer {
+  height: 60px;
+}
+.title {
+  color: #333;
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
--- apps/frontend/src/views/FederatedVoting.vue
+++ apps/frontend/src/views/FederatedVoting.vue
@@ -12,33 +12,7 @@
     </div>
     <div class="row">
       <div class="col-md-6">
-        <div class="card graph">
-          <div class="card-body pt-4 pb-0">
-            <div
-              class="d-flex justify-content-around align-items-baseline border-bottom pt-0 pb-4"
-            >
-              <div class="title mb-0 pb-0">Trust Graph</div>
-            </div>
-            <Graph
-              ref="graph"
-              :selected-vertices="selectedVertices"
-              style="height: 400px"
-              :full-screen="false"
-              :view-graph="viewGraph"
-              :is-loading="false"
-              :option-show-failing-edges="true"
-              :option-highlight-trusting-nodes="true"
-              :option-highlight-trusted-nodes="true"
-              :option-show-regular-edges="true"
-              :option-transitive-quorum-set-only="false"
-              :zoom-enabled="false"
-              :initial-zoom="2"
-              :propagation-enabled="true"
-              @vertex-selected="handleVertexSelected"
-            />
-          </div>
-          <div class="card-footer pt-0 footer"></div>
-        </div>
+        <TrustGraph />
       </div>
       <div class="col-md-6">
         <overlay-graph-base class="card-spacing" />
@@ -52,33 +26,10 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, computed } from "vue";
 import NodesPanel from "@/components/federated-voting/nodes-panel.vue";
 import SimulationControl from "@/components/federated-voting/simulation-control/simulation-control.vue";
 import OverlayGraphBase from "@/components/federated-voting/overlay-graph-base.vue";
-import Graph from "@/components/visual-navigator/graph/graph.vue";
-import ViewGraph from "@/components/visual-navigator/graph/view-graph";
-import { Network, TrustGraph } from "shared";
-import ViewVertex from "@/components/visual-navigator/graph/view-vertex";
-import { federatedVotingStore } from "@/store/useFederatedVotingStore";
-
-const viewGraph = ref<ViewGraph>(new ViewGraph());
-const selectedVertices = ref<ViewVertex[]>([]);
-
-const trustGraph = federatedVotingStore.network.nodesTrustGraph;
-
-onMounted(() => {
-  viewGraph.value = ViewGraph.fromNodes(
-    federatedVotingStore.network as Network,
-    trustGraph as TrustGraph,
-    viewGraph.value,
-  );
-});
-
-const handleVertexSelected = (vertex: ViewVertex) => {
-  selectedVertices.value = [vertex];
-  federatedVotingStore.selectedNodeId = vertex.key;
-};
+import TrustGraph from "@/components/federated-voting/trust-graph/trust-graph.vue";
 </script>
 
 <style scoped>
